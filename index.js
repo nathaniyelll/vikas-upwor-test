b613b45048897af53de110bd2c47139c64b759e2
Nathaniyell <essien.nathan@yahoo.com>
2024-03-07 13:33:18 +0100

    update index.html and style.css

---
+++ index.js
@@ -0,0 +1,7 @@
+const accordionButton = $(".accordion-button");
+const accordionBody = $(".accordion-accordion-collapse");
+
+accordionButton.click(function() {
+    // accordionBody.toggleClass("collapse");
+    console.log("clicked");
+});
