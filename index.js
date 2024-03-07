52e777daf2729e858bea13bde0e811f745d0a021
Nathaniyell <essien.nathan@yahoo.com>
2024-03-07 16:42:25 +0100

    update 5 files

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-const accordionButton = $(".accordion-button");
-const accordionBody = $(".accordion-accordion-collapse");
 
-accordionButton.click(function() {
-    // accordionBody.toggleClass("collapse");
-    console.log("clicked");
-});
+
+
